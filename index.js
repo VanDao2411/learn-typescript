919bf13cc53a3e56d89a3cd874bcae1ebcc1959c
Dao <doanvandao2411@gmai.com>
2024-05-29 11:24:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var channeName = " She is name My Hanh";
+console.log("She is girl friend me.", channeName);
---
+++ index.ts
@@ -0,0 +1,2 @@
+const channeName: String = "She is name My Hanh"
+console.log("She is girl friend me.",channeName)
