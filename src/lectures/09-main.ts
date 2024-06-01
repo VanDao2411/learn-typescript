597c7f5746c887ea439539b9f13e23f7495be0c2
Dao <doanvandao2411@gmai.com>
2024-06-01 10:15:12 +0700

    video

---
+++ src/lectures/09-main.ts
@@ -0,0 +1,15 @@
+const API_URL = 'https://api.com';
+const BACKGROUND_URL = 'https://image.com'
+
+
+let count = 1;
+count++;
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let numberList = [1, 2, 3];
+numberList = [];
+// Luôn ưu tiên dùng const , đổi sang let khi cần thiết.
+// 
